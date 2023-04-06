beab48cbea3af864e600fd1369f210a651ac2131
wangjinzhe <wangjinzhe@inspur,com>
2023-04-06 11:09:01 +0800

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {RootComponent } from '../app/shaixuanceshiform/components/root-component/rootcomponent'
+import {RootComponent } from './shaixuanceshiform/components/root-component/rootcomponent'
 const routes: Routes = [
   // { path: 'shaixuanceshiForm', loadChildren:'./shaixuanceshiform/shaixuanceshiform.module#shaixuanceshiFormModule'}
-  { path: 'shaixuanceshiForm', component:RootComponent}
+  { path: '', component:RootComponent}
   // { path: 'shaixuanceshiForm2', loadChildren:'./shaixuanceshiform2/shaixuanceshiform2.module#shaixuanceshiForm2Module'},
   // { path: 'shaixuanceshiForm3', loadChildren:'./shaixuanceshiform3/shaixuanceshiform3.module#shaixuanceshiForm3Module'},
   // { path: 'shaixuanceshiForm4', loadChildren:'./shaixuanceshiform4/shaixuanceshiform4.module#shaixuanceshiForm4Module'}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  // template: '<router-outlet></router-outlet>',
+  template: '<app-rootcomponent></app-rootcomponent>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
